d167b32f9e0eb72d50e1af87893bd297a20c05f3
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2020-08-04 10:59:00 -0300

    Cpf verification

--- src/main/java/natrodrigues/natbank/server/repository/UserRepository.java
+++ src/main/java/natrodrigues/natbank/server/repository/UserRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import natrodrigues.natbank.server.models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByCpf(String cpf);
 }
